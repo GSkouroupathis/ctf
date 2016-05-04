5582b8df3a12f1cc1f8ac61d14616af975609ec8
George Skouroupathis <georskou@cisco.com>
2016-05-04 01:14:05 +0100

    Add mystery man and Wikipedia knew (web pactf)

---
+++ pactf2016/web/mystery-man.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Searched PGP keyservers for given key ID
+
+echo Searching for $1
+declare -a arr=("adler.dlrg.de" "belgium.keyserver.net" "ds.carnet.hr" "gnv.us.ks.cryptnet.net" "keys.iif.hu" "keys.pgpi.net" "keyserver.kjsl.com" "keyserver.linux.it" "keyserver.topnet.de" "minf.vub.ac.be" "ms.pgp.cz" "palunko.srce.hr" "pgp.dtype.org" "pgp.es.net" "pgp.es.net" "pgp.escomposlinux.org" "pgp.loxinfo.co.th" "pgp.lsi.upc.es" "pgp.mit.edu" "pgp.ndlug.nd.edu" "pgp.nic.ad.jp" "pgp.rasip.fer.hr" "pgp.rediris.es" "pgp.surfnet.nl" "pgp.uk.demon.net" "pgp.uk.demon.net" "pgp.uni-mainz.de" "pgp.zdv.uni-mainz.de" "pgpkeys.mit.edu" "pgpkeys.tuwien.ac.at" "pks.pgp.cz" "stinkfoot.org" "the.earth.li" "www.keyserver.de" "www.keyserver.net" "www.rediris.es" "wwwkeys.at.pgp.net" "wwwkeys.ch.pgp.net" "wwwkeys.de.pgp.net" "wwwkeys.es.pgp.net" "wwwkeys.eu.pgp.net" "wwwkeys.nl.pgp.net" "wwwkeys.pgp.net" "wwwkeys.uk.pgp.net" "wwwkeys.us.pgp.net")
+for server in "${arr[@]}"
+do
+serverr="hkp://$server"
+echo --------------- $serverr ---------------
+gpg --keyserver $serverr --recv-keys $1
+#46726FC6
+done
---
+++ pactf2016/web/wikipedia-knew.py
@@ -0,0 +1,40 @@
+#
+# Searches the history of the 'Flag' page
+# in Wikipedia for the word 'CTF'
+#
+import urllib2
+import re
+
+noOfLoops = 15000
+keyword = "ctf"
+pagesWithKeyword = {}
+keywordRegex = re.compile('[\s\d\D]*' + keyword + '[\s\d\D]*', re.IGNORECASE)
+
+lastPage = "https://en.wikipedia.org/w/index.php?title=Flag&oldid=718031133"
+
+for i in range(noOfLoops):
+    print i,
+    response = urllib2.urlopen(lastPage)
+    htmlSource = response.read()
+
+    # Find keyword
+    if keywordRegex.match(htmlSource):
+        print 'V',
+        print lastPage
+
+
+        keywordFinderRegex = re.compile(r'(' + keyword + r'[\s\d\D]*?)\n', re.IGNORECASE)
+        keywordFound = re.findall(keywordFinderRegex, htmlSource)[0]
+        print keywordFound
+        pagesWithKeyword[lastPage] = keywordFound
+
+    else:
+        print 'X',
+        print lastPage
+
+    # Get previous pages
+    previousLinkID = re.findall(r'<a href="/w/index\.php\?title=Flag&amp;direction=prev&amp;oldid=(\d+)', htmlSource)[0]
+    lastPage = 'https://en.wikipedia.org/w/index.php?title=Flag&direction=prev&oldid=' + previousLinkID
+
+for page in pagesWithKeyword:
+    print page
